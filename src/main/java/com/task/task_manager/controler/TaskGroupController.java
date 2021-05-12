49e289844246efc91821b2a2312e5a2fdca33a0a
Mateusz <mateusz.machniak@comarch.com>
2021-05-12 21:26:56 +0200

    Praca domowa

--- src/main/java/com/task/task_manager/controler/TaskGroupController.java
+++ src/main/java/com/task/task_manager/controler/TaskGroupController.java
@@ -1,9 +1,7 @@
 package com.task.task_manager.controler;
 
 import com.task.task_manager.entity.TaskGroup;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +10,32 @@ import java.util.List;
 @CrossOrigin
 public class TaskGroupController {
 
+    private final List<TaskGroup> taskGroups = new ArrayList<>();
+
+    public TaskGroupController() {
+        taskGroups.add(new TaskGroup(1L, "Promocja Santander v2"));
+        taskGroups.add(new TaskGroup(2L, "Promocja GetIn"));
+        taskGroups.add(new TaskGroup(3L, "Promocja Milenium"));
+        taskGroups.add(new TaskGroup(3L, "Promocja Milenium 3"));
+    }
+
     @GetMapping("/taskGroup")
     public List<TaskGroup> getTaskGroups() {
-        List<TaskGroup> list = new ArrayList<>();
-        list.add(new TaskGroup(1L, "Promocja Santander v2"));
-        list.add(new TaskGroup(2L, "Promocja GetIn"));
-        list.add(new TaskGroup(3L, "Promocja Milenium"));
-        return list;
+        return taskGroups;
     }
+
+    @GetMapping("/taskGroup/{id}")
+    public TaskGroup getTaskGroup(@PathVariable Long id) {
+        //zwrocic obiekt po id
+        return null;
+    }
+
+
+    @DeleteMapping("/taskGroup/{id}")
+    public void deleteTaskGroup(@PathVariable Long id) {
+        //usunac na podstawie id
+    }
+
+
 
 }
