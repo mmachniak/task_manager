5300468596e66fe7f51a9eddc14dd3d6ec08a1e0
Mateusz <mateusz.machniak@comarch.com>
2021-05-13 13:38:56 +0200

    Refactor zadania

--- src/main/java/com/task/task_manager/controler/TaskGroupController.java
+++ src/main/java/com/task/task_manager/controler/TaskGroupController.java
@@ -4,6 +4,7 @@ import com.task.task_manager.entity.TaskGroup;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
@@ -26,19 +27,34 @@ public class TaskGroupController {
 
     @GetMapping("/taskGroup/{id}")
     public TaskGroup getTaskGroup(@PathVariable Long id) {
-        for (TaskGroup taskGroup : taskGroups) {
-            if (taskGroup.getId().equals(id)) return taskGroup;
-        }
-        //zwrocic obiekt po id
-        return null;
+        return taskGroups.stream()
+                .filter(element -> element.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+//        for (TaskGroup taskGroup : taskGroups) {
+//            if (taskGroup.getId().equals(id)) return taskGroup;
+//        }
+//        //zwrocic obiekt po id
+//        return null;
     }
 
 
     @DeleteMapping("/taskGroup/{id}")
     public void deleteTaskGroup(@PathVariable Long id) {
-        for (int i = 0; i < taskGroups.size(); i++){
-            if (taskGroups.get(i).getId().equals(id)) taskGroups.remove(i);
-        }
+        taskGroups.removeIf(element -> element.getId().equals(id));
+//        Iterator<TaskGroup> iterator = taskGroups.iterator();
+//        while (iterator.hasNext()) {
+//            TaskGroup next = iterator.next();
+//            if (next.getId().equals(id)) {
+//                iterator.remove();
+//            }
+//        }
+
+//        for (int i = 0; i < taskGroups.size(); i++){
+//            if (taskGroups.get(i).getId().equals(id)) {
+//                taskGroups.remove(i);
+//            }
+//        }
         //usunac na podstawie id
     }
 
