81bd675098921afae9a1469f14dd076a51a86d8b
Mateusz <mateusz.machniak@comarch.com>
2021-05-18 12:09:09 +0200

    Auto remove tasks

--- src/main/java/com/task/taskmanager/entity/TaskGroup.java
+++ src/main/java/com/task/taskmanager/entity/TaskGroup.java
@@ -3,6 +3,7 @@ package com.task.taskmanager.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -18,7 +19,7 @@ public class TaskGroup {
   private String name;
 
   @JsonIgnore
-  @OneToMany(mappedBy = "taskGroup")
+  @OneToMany(mappedBy = "taskGroup", orphanRemoval = true)
   private List<Task> tasks = new ArrayList<>();
 
   protected TaskGroup() {
